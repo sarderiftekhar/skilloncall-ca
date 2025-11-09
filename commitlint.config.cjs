72a1564e848d8f2df6fbca836cfb922d5c538ba5
Sarder Iftekhar <sarder2008@gmail.com>
2025-11-09 23:11:59 +0000

    Remove Husky, commitlint, and lint-staged from the project

--- commitlint.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        // Disable the problematic rules temporarily
-        'subject-case': [0],
-        'header-max-length': [2, 'always', 120],
-        'type-enum': [0],
-    },
-};
